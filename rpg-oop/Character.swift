e992cb0695d66be926d677f856136cfa02f13b20
Jonathan Wood <jonathankimballwood@gmail.com>
2015-09-05 17:04:39 -0600

    initial commit

---
+++ rpg-oop/Character.swift
@@ -0,0 +1,15 @@
+//
+//  character.swift
+//  rpg-oop
+//
+//  Created by Jonathan Wood on 9/5/15.
+//  Copyright © 2015 Jonathan Wood. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp: Int = 100
+    private var 
+    
+}
---
+++ rpg-oop/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  rpg-oop
+//
+//  Created by Jonathan Wood on 9/5/15.
+//  Copyright © 2015 Jonathan Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
